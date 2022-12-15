2af6c9040814b73eff59d3fabc401bb2b7b8d10a
thantrieu <trieuthan92pro@gmail.com>
2022-12-15 15:10:59 +0700

    Initialize Android Project

---
+++ app/src/main/java/net/braniumacademy/learngit/Student.java
@@ -0,0 +1,14 @@
+package net.braniumacademy.learngit;
+
+import java.util.Date;
+
+public class Student {
+    private String studentId;
+    private String fullName;
+    private float gpa;
+    private Date birthDate;
+}
+// git add <tên file>
+// git add <tên thư mục>
+// git add *.txt
+// git add .
