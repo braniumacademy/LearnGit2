39350e15df6331a1b9526afab3ad84f6d8c246c7
thantrieu <trieuthan92pro@gmail.com>
2022-12-16 15:49:42 +0700

    Add some fields and constructor for class Test

--- app/src/main/java/net/braniumacademy/learngit/Test.java
+++ app/src/main/java/net/braniumacademy/learngit/Test.java
@@ -1,4 +1,19 @@
 package net.braniumacademy.learngit;
 
 public class Test {
+    private int age;
+    private String fullName;
+    private String job;
+    private String address;
+
+    public Test(int age) {
+        this.age = age;
+    }
+
+    public Test(int age, String fullName, String job, String address) {
+        this.age = age;
+        this.fullName = fullName;
+        this.job = job;
+        this.address = address;
+    }
 }
