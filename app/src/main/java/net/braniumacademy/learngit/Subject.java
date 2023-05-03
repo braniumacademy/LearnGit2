21decadb51572f3fe5f1c853d159359b1d8385d0
thantrieu <trieuthan92pro@gmail.com>
2023-05-03 16:33:51 +0700

    Add update file Subject

--- app/src/main/java/net/braniumacademy/learngit/Subject.java
+++ app/src/main/java/net/braniumacademy/learngit/Subject.java
@@ -1,5 +1,17 @@
 package net.braniumacademy.learngit;
 
 public class Subject {
+    private String subjectId;
 
+    public Subject() {
+        subjectId= "";
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
 }
