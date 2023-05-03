2687c692c36790b722a6f809f9aae1cfe28aca49
thantrieu <trieuthan92pro@gmail.com>
2023-05-03 19:03:46 +0700

    Update controllers

--- app/src/main/java/net/braniumacademy/learngit/controller/StudentController.java
+++ app/src/main/java/net/braniumacademy/learngit/controller/StudentController.java
@@ -1,4 +1,11 @@
 package net.braniumacademy.learngit.controller;
 
+import net.braniumacademy.learngit.Student;
+
+import java.util.List;
+
 public class StudentController {
+    public List<Student> findStudentByName(String name) {
+        return null;
+    }
 }
