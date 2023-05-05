bba8751268e0a9490557034ef70fdd01cf6d76b6
thantrieu <trieuthan92pro@gmail.com>
2023-05-05 10:40:06 +0700

    Add package util

---
+++ app/src/main/java/net/braniumacademy/learngit/util/Others.java
@@ -0,0 +1,13 @@
+package net.braniumacademy.learngit.util;
+
+public class Others {
+    private String something;
+
+    public String getSomething() {
+        return something;
+    }
+
+    public void setSomething(String something) {
+        this.something = something;
+    }
+}
