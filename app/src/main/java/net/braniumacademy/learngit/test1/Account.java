ea89925978485b53381f623dc5a4099e4eaf6c1a
thantrieu <trieuthan92pro@gmail.com>
2023-05-05 11:22:11 +0700

    Add Account

---
+++ app/src/main/java/net/braniumacademy/learngit/test1/Account.java
@@ -0,0 +1,31 @@
+package net.braniumacademy.learngit.test1;
+
+public class Account {
+    private String email;
+    private String id;
+    private String username;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
