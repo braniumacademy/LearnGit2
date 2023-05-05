6fb747bfcbad349f52a2ca716eb2b34daebeb3c0
thantrieu <trieuthan92pro@gmail.com>
2023-05-05 10:07:11 +0700

    Add update Master

---
+++ app/src/main/java/net/braniumacademy/learngit/Master.java
@@ -0,0 +1,13 @@
+package net.braniumacademy.learngit;
+
+public class Master {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
