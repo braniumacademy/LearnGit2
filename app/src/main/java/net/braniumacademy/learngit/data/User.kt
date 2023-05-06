1b2b52f6b15b2f2ec2ba498dc0d50ba25e1a5dd2
thantrieu <trieuthan92pro@gmail.com>
2023-05-06 10:03:01 +0700

    Update User

--- app/src/main/java/net/braniumacademy/learngit/data/User.kt
+++ app/src/main/java/net/braniumacademy/learngit/data/User.kt
@@ -1,8 +1,9 @@
 package net.braniumacademy.learngit.data
 
 data class User(
-    val id: Int,
+    val id: Long,
     val firstName: String,
     val lastName: String,
     val email: String
+    // this is an inline comment
 )
